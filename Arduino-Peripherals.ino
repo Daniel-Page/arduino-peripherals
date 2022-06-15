981560e2c9a6422aaa96a019cff0ed1aaf51d08d
Daniel Page <73043391+Daniel-Page@users.noreply.github.com>
2022-06-15 19:50:40 +1200

    Create Arduino-Peripherals.ino

---
+++ Arduino-Peripherals.ino
@@ -0,0 +1,45 @@
+/* 
+ *  Arduino Peripherals
+ */
+
+
+#include "io.h"
+#include "msg.h"
+#include "defs.h" // Must be last include
+
+
+// Class object initialisations
+led RedLED(RED_LED_PIN_ADR, OUTPUT);
+led BlueLED(BLUE_LED_PIN_ADR, OUTPUT);
+button RedButton(RED_BUT_PIN_ADR, INPUT_PULLUP);
+button BlueButton(BLUE_BUT_PIN_ADR, INPUT_PULLUP);
+pot Pot(POT_PIN_ADR, INPUT, ADC_BITS, VCC);
+
+msg Message;
+
+
+void setup() {
+  Message.init(); // Has to be in setup
+  invertTrigger(2, &RedButton, &BlueButton); // Swaps between active high and active low
+}
+
+
+void loop() {
+  pollInputs(2, &RedButton, &BlueButton); // Updates the state variables for inputs
+
+  if (RedButton.getState()) {
+    RedLED.on(); 
+  } else {
+    RedLED.off();
+  }
+  
+  if (BlueButton.getState()) {
+    BlueLED.on();
+  } else {
+    BlueLED.off();
+  }
+
+  Message.stageBut(2, &RedButton, &BlueButton); // Converts all possible input states into a unique number
+  Message.stagePot(1, &Pot);
+  Message.transmit(); // Send composed message via serial and reset buffer
+}
